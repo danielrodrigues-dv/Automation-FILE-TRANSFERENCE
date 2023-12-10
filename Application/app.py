747ff9484fefffb3f30c965d30d5b2a4647e2401
Daniel Rodrigues <41621213+danielrodrigues-dv@users.noreply.github.com>
2023-12-10 00:56:29 -0300

    Add files via upload

---
+++ Application/app.py
@@ -0,0 +1,10 @@
+import pyautogui
+
+pyautogui.moveTo(1191, 132, duration=0.5)
+pyautogui.click()
+pyautogui.hotkey('ctrl', 'a')
+pyautogui.hotkey('ctrl', 'x')
+pyautogui.moveTo(1175,715, duration=0.5)
+pyautogui.click()
+pyautogui.hotkey('ctrl', 'v')
+   
